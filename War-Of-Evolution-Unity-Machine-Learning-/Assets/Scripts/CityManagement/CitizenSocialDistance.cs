06220c478f5bedaa26398af050d8f98aff86e1b0
Yusuftree <yusuftree1@hotmail.com>
2023-01-03 13:26:59 +0300

    update: Smooth Social Distance

--- War-Of-Evolution-Unity-Machine-Learning-/Assets/Scripts/CityManagement/CitizenSocialDistance.cs
+++ War-Of-Evolution-Unity-Machine-Learning-/Assets/Scripts/CityManagement/CitizenSocialDistance.cs
@@ -10,25 +10,27 @@ public class CitizenSocialDistance : MonoBehaviour
     public float force = 10f;
     public float friction = 0.5f;
     public float socialDistance = 1f;
+    
+    private CityManagement cityManagement;
 
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cityManagement = FindObjectOfType<CityManagement>();
     }
     
     void FixedUpdate()
     {
-        Collider[] hits = Physics.OverlapSphere(transform.position, socialDistance, citizenMask);
+        Collider[] hits = Physics.OverlapSphere(transform.position, socialDistance * cityManagement.citySocialDistanceMultiplier, citizenMask);
         foreach (Collider hit in hits)
         {
             if (hit.transform.GetInstanceID() != transform.GetInstanceID() && hit.transform.CompareTag("Citizen"))
             {
                 Vector3 direction = hit.transform.position - transform.position;
-                Debug.Log("Hit position: " + hit.transform.position + " Self position: " + transform.position);
-                hit.GetComponent<Rigidbody>().AddForce(direction.normalized * force, ForceMode.Impulse);
-                Debug.Log("Name " + hit.name + " / Direction " + direction);
+                hit.GetComponent<Rigidbody>().AddForce(direction.normalized * (force * cityManagement.citySocialDistanceForceMultiplier * (1 / direction.magnitude)), ForceMode.Force);
+                Debug.Log("Forced Name " + hit.name + " / Direction " + direction);
             }
         }
         rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, Time.deltaTime * friction);
---
+++ War-Of-Evolution-Unity-Machine-Learning-/Assets/Scripts/CityManagement/CityManagement.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityManagement : MonoBehaviour
+{
+    public float citySocialDistanceMultiplier = 1;
+    public float citySocialDistanceForceMultiplier = 1;
+}
